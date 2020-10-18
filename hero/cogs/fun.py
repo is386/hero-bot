226b0f2cd4a1c05c8251256409f3398db0016f1e
is386 <20195347+is386@users.noreply.github.com>
2020-10-17 21:15:56 -0400

    add funny stuff to roll

--- hero/cogs/fun.py
+++ hero/cogs/fun.py
@@ -8,6 +8,7 @@ meme_path: str = "databases/memes"
 heads: str = "https://i.imgur.com/dTNbMle.png"
 tails: str = "https://i.imgur.com/Suza17V.png"
 add_meme_msg: str = "I added this new meme"
+dbz_gif: str = "https://media.tenor.com/images/f1d0693271bdf3259481a1e54d184673/tenor.gif"
 
 
 class Fun(commands.Cog):
@@ -53,8 +54,18 @@ class Fun(commands.Cog):
 
     @commands.command(name="roll")
     async def roll(self, ctx: commands.Context, dice_sides: int):
-        dice_sides = dice_sides if dice_sides < 1000000000 else 1000000000
-        await ctx.send("🎲 {} rolled **{}** 🎲".format(ctx.author.mention, randint(1, dice_sides)))
+        if dice_sides in [69, 420]:
+            await ctx.send("Not funny. Didn't laugh 😐")
+        else:
+            dice_sides = dice_sides if dice_sides < 1000000000 else 1000000000
+            roll: int = randint(1, dice_sides)
+            if roll == 9001:
+                model: EmbedModel = EmbedModel("9001")
+                model.set_image(dbz_gif)
+                embed: Embed = embeds.create_embed(model)
+                await ctx.send(embed=embed)
+            else:
+                await ctx.send("🎲 {} rolled **{}** 🎲".format(ctx.author.mention, roll))
 
     @add_meme.error
     async def perm_error(self, ctx: commands.Context, error: commands.CommandError):

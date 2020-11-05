29168d53894f06ac7727f937f2705c2cef0e9a65
is386 <20195347+is386@users.noreply.github.com>
2020-11-05 12:20:49 -0500

    refactor infraction cmds

--- hero/cogs/mod.py
+++ hero/cogs/mod.py
@@ -1,3 +1,6 @@
+from hero.utils.reactions import confirm
+from typing import List
+
 from discord import Embed, Member, Message, utils, Role,  VoiceChannel, TextChannel, PermissionOverwrite
 from discord.abc import GuildChannel
 from discord.ext import commands
@@ -7,12 +10,13 @@ from hero.utils.embed_model import EmbedModel
 
 bad_user: str = "That user does not exist. Did you try pinging the user?"
 mute_role: str = "Snooze"
-mute_role: str = "Snooze"
+
 text_mute_perms: PermissionOverwrite = PermissionOverwrite()
 text_mute_perms.send_messages = False
 text_mute_perms.send_tts_messages = False
 text_mute_perms.attach_files = False
 text_mute_perms.add_reactions = False
+
 voice_mute_perms: PermissionOverwrite = PermissionOverwrite()
 voice_mute_perms.connect = False
 
@@ -36,83 +40,50 @@ class Mod(commands.Cog):
     @commands.command(name="kick")
     @commands.has_permissions(kick_members=True)
     async def kick(self, ctx: commands.Context, *args):
-        if len(args) < 2:
-            await ctx.send("That's not right. The format is `?kick <ping> <reason>`.")
-            return
-
-        user: Member = self.parse_mention(ctx, args[0])
-        if not user:
-            await ctx.send(bad_user)
-            return
-
-        reason: str = " ".join(args[1:])
-        embed: Embed = self.get_infraction_embed(user, reason, "kick")
-        resp: Message = await ctx.send(embed=embed)
-        # Waits for a mod to confirm their choice before kicking
-        confirm_kick: bool = await reactions.confirm(ctx, resp)
-
-        if confirm_kick:
+        user, reason, confirm = await self.prep_infraction(ctx, args, "kick")
+        if confirm:
             await user.kick(reason=reason)
             await ctx.send("**{}** was kicked for **{}**.".format(user.name, reason))
-        else:
+        elif user and not confirm:
             await ctx.send("The kick was cancelled.")
 
     # Bans a user from the server
     @commands.command(name="ban")
     @commands.has_permissions(ban_members=True)
     async def ban(self, ctx: commands.Context, *args):
-        if len(args) < 2:
-            await ctx.send("That's not right. The format is `?ban <ping> <reason>`.")
-            return
-
-        user: Member = self.parse_mention(ctx, args[0])
-        if not user:
-            await ctx.send(bad_user)
-            return
-
-        reason: str = " ".join(args[1:])
-        embed: Embed = self.get_infraction_embed(user, reason, "ban")
-        resp: Message = await ctx.send(embed=embed)
-        # Waits for a mod to confirm their choice before banning
-        confirm_ban: bool = await reactions.confirm(ctx, resp)
-
-        if confirm_ban:
+        user, reason, confirm = await self.prep_infraction(ctx, args, "ban")
+        if confirm:
             await user.ban(reason=reason)
             await ctx.send("**{}** was banned for **{}**.".format(user.name, reason))
-        else:
+        elif user and not confirm:
             await ctx.send("The ban was cancelled.")
+
+    # Bans a user from the server
+    @commands.command(name="warn")
+    @commands.has_permissions(kick_members=True)
+    async def warn(self, ctx: commands.Context, *args):
+        user, reason, confirm = await self.prep_infraction(ctx, args, "warn")
+        if confirm:
+            await ctx.send("**{}** was warned for **{}**.".format(user.name, reason))
+        elif user and not confirm:
+            await ctx.send("The warn was cancelled.")
 
     # Mutes the given user
     @commands.command(name="mute", aliases=["snooze"])
     @commands.has_permissions(manage_roles=True)
     async def mute(self, ctx, *args):
-        if len(args) < 2:
-            await ctx.send("That's not right. The format is `?mute <ping> <reason>`.")
-            return
-
-        user: Member = self.parse_mention(ctx, args[0])
-        if not user:
-            await ctx.send(bad_user)
-            return
-
-        if utils.get(user.roles, name=mute_role):
-            await ctx.send("This user is already affliced by snooze.")
-            return
-
-        reason: str = " ".join(args[1:])
-        embed: Embed = self.get_infraction_embed(user, reason, "mute")
-        resp: Message = await ctx.send(embed=embed)
-        # Waits for a mod to confirm their choice before muting
-        confirm_mute: bool = await reactions.confirm(ctx, resp)
-
-        if confirm_mute:
+        user, reason, confirm = await self.prep_infraction(ctx, args, "mute")
+        if confirm:
+            if utils.get(user.roles, name=mute_role):
+                await ctx.send("This user is already afflicted by snooze.")
+                return
             role: Role = utils.get(user.guild.roles, name=mute_role)
             # Creates the role if it doesn't exist
             if not role:
                 role = await user.guild.create_role(name=mute_role)
             await user.add_roles(role)
             await ctx.send("**{}** was snoozed for **{}**.".format(user.name, reason))
-        else:
+        elif user and not confirm:
             await ctx.send("Snooze missed.")
 
     # Unmutes the given user
@@ -143,26 +114,22 @@ class Mod(commands.Cog):
     async def on_guild_channel_create(self, channel: GuildChannel):
         await self.add_mute_role(channel)
 
-    @kick.error
-    @ban.error
-    @mute.error
-    @unmute.error
-    async def ban_error(self, ctx: commands.Context, error: commands.CommandError):
-        if isinstance(error, commands.MissingPermissions):
-            await ctx.send("{} you do not have permission to do that!".format(ctx.author.mention))
-        elif isinstance(error, commands.MissingRequiredArgument):
-            await ctx.send(error)
-        else:
-            await ctx.send("They are too powerful...")
+    # Prepares an infraction by checking command form, user id, and sending a confirm message.
+    async def prep_infraction(self, ctx: commands.Context, args: List[str], cmd: str) -> bool:
+        if len(args) < 2:
+            await ctx.send("That's not right. The format is `?{} <ping> <reason>`.".format(cmd))
+            return None, None, None
 
-    @slow_mode.error
-    async def slow_mode_error(self, ctx: commands.Context, error: commands.CommandError):
-        if isinstance(error, commands.MissingPermissions):
-            await ctx.send("{} you do not have permission to do that!".format(ctx.author.mention))
-        elif isinstance(error, commands.MissingRequiredArgument):
-            await ctx.send(error)
-        else:
-            await ctx.send("That is not a number.")
+        user: Member = self.parse_mention(ctx, args[0])
+        if not user:
+            await ctx.send(bad_user)
+            return None, None, None
+
+        reason: str = " ".join(args[1:])
+        embed: Embed = self.get_infraction_embed(user, reason, cmd)
+        resp: Message = await ctx.send(embed=embed)
+        # Waits for a mod to confirm their choice before banning
+        return user, reason, await reactions.confirm(ctx, resp)
 
     # Parses the userid from a mention and returns a Member object
     def parse_mention(self, ctx: commands.Context, mention: str) -> Member:
@@ -191,3 +158,24 @@ class Mod(commands.Cog):
             await channel.set_permissions(role, overwrite=text_mute_perms)
         elif isinstance(channel, VoiceChannel):
             await channel.set_permissions(role, overwrite=voice_mute_perms)
+
+    @kick.error
+    @ban.error
+    @mute.error
+    @unmute.error
+    async def ban_error(self, ctx: commands.Context, error: commands.CommandError):
+        if isinstance(error, commands.MissingPermissions):
+            await ctx.send("{} you do not have permission to do that!".format(ctx.author.mention))
+        elif isinstance(error, commands.MissingRequiredArgument):
+            await ctx.send(error)
+        else:
+            await ctx.send("They are too powerful...")
+
+    @slow_mode.error
+    async def slow_mode_error(self, ctx: commands.Context, error: commands.CommandError):
+        if isinstance(error, commands.MissingPermissions):
+            await ctx.send("{} you do not have permission to do that!".format(ctx.author.mention))
+        elif isinstance(error, commands.MissingRequiredArgument):
+            await ctx.send(error)
+        else:
+            await ctx.send("That is not a number.")
